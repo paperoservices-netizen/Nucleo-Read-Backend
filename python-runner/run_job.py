cf47412aed084ef07c857e80d8039872da75e40c
Anupriya2508 <anu.priya.jayaraj08@gmail.com>
2026-02-04 16:15:46 +0530

    update

--- python-runner/run_job.py
+++ python-runner/run_job.py
@@ -1,8 +1,16 @@
-import sys, os, json, base64
+import sys, os, json
 from robio import analyze
 
-fasta = base64.b64decode(sys.argv[1]).decode("utf-8")
-job_id = sys.argv[2]
+job_id = sys.argv[1]
+
+input_file = f"input/{job_id}.fasta"
+
+if not os.path.exists(input_file):
+    print("Input FASTA not found:", input_file)
+    exit(1)
+
+with open(input_file, "r") as f:
+    fasta = f.read()
 
 os.makedirs("python-runner/results", exist_ok=True)
 
